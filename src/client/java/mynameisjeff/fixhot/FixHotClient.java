5b94b5576c4b277eb44854999e63f31f7e71e881
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2025-04-06 23:46:02 -0400

    Initial Fabric Template

---
+++ src/client/java/mynameisjeff/fixhot/FixHotClient.java
@@ -0,0 +1,10 @@
+package mynameisjeff.fixhot;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FixHotClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
